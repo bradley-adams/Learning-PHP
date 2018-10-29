77f6bfd2b2b4a93cae3a483099592b664f9a3e47
Bradley Adams <bradley.wayne.adams@gmail.com>
2018-10-30 11:12:54 +1300

    basic math

---
+++ L11/math.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+echo 56 + 45;
+echo "<br>";
+echo 56 - 45;
+echo "<br>";
+echo 56 * 45;
+echo "<br>";
+echo 56 / 45; //creates a floating number = 1.244444etc
+echo "<br>";
+echo "<br>";
+
+echo 45 + 34 * 45 / 421 - 45;
+echo "<br>";
+
+echo (5 + 5) / 10; //order of operations
+echo "<br>";
+echo 5 + 5 / 10;
+echo "<br>";
+
+$number1 = 12;
+$number2 = 24;
+
+echo $number1 + $number2;
+echo "<br>";
+echo $number1 * $number2;
+
+
+?>
+
+</body>
+</html>
