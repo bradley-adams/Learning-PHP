3d1a058621ba562afabf1ad67c6fcdc7b59f4723
Bradley Adams <bradley.wayne.adams@gmail.com>
2018-10-30 13:55:04 +1300

    php if statements

---
+++ L15/ifstatement.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+if(3 > 10){
+
+    echo "three is less then 10";
+
+} elseif(4 > 5){
+
+    echo "of course four is less that 5";
+
+} else {
+
+    echo "it is not";
+}
+
+?>
+
+</body>
+</html>
