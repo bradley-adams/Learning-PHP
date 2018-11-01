9ea9e135d911eb9f41949607cc37ed660e4564ef
Bradley Adams <bradley.wayne.adams@gmail.com>
2018-11-01 21:42:31 +1300

    basic validations from forms

--- L32-L33/form.php
+++ L32-L33/form.php
@@ -3,11 +3,29 @@
 
 if(isset($_POST['submit'])){
 
+    $name = array("Edwin", "Bradley", "Peter", "Samid", "Mohad", "Jane", "Tom");
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-echo "Hello " . $username;
-echo ", your password is: " . $password;
+$minimum = 5;
+$maximum = 10;
+
+if(strlen($username) < $minimum){
+    echo "Username needs to be longer then 5 characters" . "<br>";
+}
+if(strlen($username) > $maximum){
+        echo "Username cannot be longer then 10 characters" . "<br>";
+}
+
+if(!in_array($username, $name)){
+ echo "You. Shall. Not. Pass.";
+} else {
+    echo "Welcome";
+}
+
+//echo "Hello " . $username;
+//echo ", your password is: " . $password;
 
 }
 
