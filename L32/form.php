4943fcafd27e307164473a55d6b73af443e8504a
Bradley Adams <bradley.wayne.adams@gmail.com>
2018-11-01 17:26:11 +1300

    forms with php

---
+++ L32/form.php
@@ -0,0 +1,31 @@
+
+<?php
+
+if(isset($_POST['password'])){
+
+echo "password works";
+
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="form.php" method="post">
+    <input type ="text" placeholder="Username" name="username">
+    <br>
+    <input type ="password" placeholder="Password" name="password">
+    <br>
+    <input type ="submit" name="submit">
+</form>
+
+</body>
+</html>
