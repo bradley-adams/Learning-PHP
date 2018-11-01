1e331ed9a389e6dff9ad7b03e64e03b362651765
Bradley Adams <bradley.wayne.adams@gmail.com>
2018-11-01 14:11:14 +1300

    built in PHP math functions

---
+++ L28/math_functions.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+echo pow(2, 7); // 2 to the 7th power
+echo "<br>";
+echo rand(1, 1000); //give me a random number between 1 and 1000.
+echo "<br>";
+echo sqrt(100); //square root of 100
+echo "<br>";
+echo ceil(4.6); // round it up to the next integer.
+echo "<br>";
+echo floor(4.6); // round down
+echo "<br>";
+echo round(4.5); //standard rounding. Up if the decimal is .5 or higher. Down if the decimal is .4 or lower.
+
+?>
+
+</body>
+</html>
