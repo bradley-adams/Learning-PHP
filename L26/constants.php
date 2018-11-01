d4be39d5f6f83862d3689d800c1c760cad7eedba
Bradley Adams <bradley.wayne.adams@gmail.com>
2018-11-01 13:08:33 +1300

    defining a constant with php

---
+++ L26/constants.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$number = 10;
+$number = 27; // you can change a variable
+echo $number . "<br>";
+
+define("Name", "Brad"); // how we define a constant. First variable is the constant name and second is the value.
+// you cannot change a constant
+
+echo Name . "<br>";
+
+echo Name . " is " . $number . " years old."
+
+?>
+
+</body>
+</html>
