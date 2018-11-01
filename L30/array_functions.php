3db0d3a3fae8b2cc0f903e2bc803f66594e940e4
Bradley Adams <bradley.wayne.adams@gmail.com>
2018-11-01 15:32:10 +1300

    some built in array functions

---
+++ L30/array_functions.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+//http://php.net/manual/en/ref.array.php
+
+$list = [343, 34, 323, 23, 54, 232, 453];
+
+echo max($list);
+echo "<br>";
+echo min($list);
+echo "<br>";
+
+sort($list);
+print_r($list);
+echo "<br>";
+
+
+
+
+?>
+
+</body>
+</html>
