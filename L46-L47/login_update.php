3a5f0f69c47c644fcb1cf81b4a422512cd653de3
Bradley Adams <bradley.wayne.adams@gmail.com>
2018-11-03 15:07:09 +1300

    Update Records form and DB file

--- L46-L47/login_update.php
+++ L46-L47/login_update.php
@@ -34,7 +34,7 @@
         </div>
 
       <div class="form-group">
-          <select name="" id="">
+          <select name="id" id="">
               <option>1</option>
           </select>
       </div>
