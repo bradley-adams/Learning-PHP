c79d81aec937fe3f7ea005155b86d6d60224824d
Bradley Adams <bradley.wayne.adams@gmail.com>
2018-11-08 17:41:42 +1300

    section 10 exercise

---
+++ L75/10.php
@@ -0,0 +1,55 @@
+<?php include "functions.php"; ?>
+<?php include "includes/header.php";?>
+<section class="content">
+
+	<aside class="col-xs-4">
+
+		<?php Navigation();?>
+			
+			
+	</aside><!--SIDEBAR-->
+
+<article class="main-content col-xs-8">
+
+	
+	<?php
+    class Dog{
+        public $eyeColor = "Blue";
+        protected $nose = "Flat";
+        private $fur = "Fluffy";
+        var $name = "Dog";
+
+        function showAll(){
+            echo $this->eyeColor ."<br>";
+            echo $this->nose ."<br>";
+            echo $this->fur ."<br>";
+            echo $this->name ."<br>";
+        }
+    }
+    $pitbull = new Dog();
+    $pitbull->showAll();
+
+	/*  Step 1: Use the Make a class called Dog
+
+		Step 2: Set some properties for Dog, Example, eye colors, nose, or fur color
+
+		Step 4: Make a method named ShowAll that echos all the properties
+
+		Step 5: Instantiate the class / create object and call it pitbull
+
+Step 6: Call the method ShowAll
+
+	
+
+		
+	*/
+	
+	?>
+
+
+
+
+
+</article><!--MAIN CONTENT-->
+
+<?php include "includes/footer.php"; ?>
